6f3ef1585997d71092320649b74b131cf9bd62b3
Samrah Syed <samrah727@gmail.com>
2023-08-23 13:29:06 +0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+const emailSI = document.getElementById("email");
+const passwordSI = document.getElementById("pwd-email");
+const upBtn = document.getElementById("up-btn");
+const inBtn = document.getElementById("in-btn");
+
+
